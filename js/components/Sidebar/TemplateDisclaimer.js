85feb0088df04c0edaff2e44a4a1b5977d1380e3
silenceglobally <gisner271@gmail.com>
2025-10-06 20:58:04 -0400

    g

--- js/components/Sidebar/TemplateDisclaimer.js
+++ js/components/Sidebar/TemplateDisclaimer.js
@@ -1,7 +1,7 @@
 export default {
     template: `
         <div class="og">
-            <p class="type-label-md"><a class="director" href="" target="_blank"></a>  <a class="director" href="" target="_blank"></a>  <a class="director" href="" target="_blank"></a>By Silentium</p>
+            <p class="type-label-md"><a class="director" href="" target="_blank"></a>  <a class="director" href="" target="_blank"></a>  <a class="director" href="" target="_blank"></a>By Silentium.</p>
         </div>
     `
 }

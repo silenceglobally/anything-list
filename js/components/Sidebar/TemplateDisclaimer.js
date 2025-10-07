cf96254e5cc8305881898bc2287b224eed5e4585
silenceglobally <gisner271@gmail.com>
2025-10-06 20:55:11 -0400

    j

--- js/components/Sidebar/TemplateDisclaimer.js
+++ js/components/Sidebar/TemplateDisclaimer.js
@@ -1,1 +1,7 @@
-
+export default {
+    template: `
+        <div class="og">
+            <p class="type-label-md"><a class="director" href="" target="_blank"></a>;  <a class="director" href="" target="_blank"></a> & <a class="director" href="" target="_blank">k</a>.</p>
+        </div>
+    `
+}
